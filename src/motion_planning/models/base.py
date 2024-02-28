2e3fcd5c69f3f761981eadc80e1cb4397aacc00b
Damian Owerko <Damowerko@users.noreply.github.com>
2024-02-28 13:04:14 -0500

    change to torchcps model

--- src/motion_planning/models/base.py
+++ src/motion_planning/models/base.py
@@ -1,92 +1,20 @@
-from typing import Any, Dict, Iterator, List, Optional, Union
+import typing
+from typing import Iterator, List, Optional
 
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch_geometric.nn as gnn
 from torch_geometric.data import Batch, Data
 from torch_geometric.data.data import BaseData
 from torch_geometric.loader import DataLoader
 from torch_geometric.utils.convert import from_scipy_sparse_matrix
 from torch_scatter import scatter_mean
+from torchcps.gnn import GCN
 from torchcps.utils import add_model_specific_args
 
 from motion_planning.envs.motion_planning import MotionPlanning
 from motion_planning.rl import ExperienceSourceDataset
-
-
-class GNN(nn.Module):
-    activations: Dict[str, nn.Module] = {
-        "relu": nn.ReLU(True),
-        "leaky_relu": nn.LeakyReLU(True),
-        "tanh": nn.Tanh(),
-    }
-
-    def __init__(
-        self,
-        F_in: int,
-        F_out: int,
-        F: int,
-        K: int,
-        n_layers_gnn: int,
-        n_layers_mlp: int,
-        dropout: float = 0.0,
-        activation: str = "leaky_relu",
-        architecture: str = "tag",
-    ):
-        super().__init__()
-        if F_in < 1 or F_out < 1 or F < 1 or K < 1:
-            raise ValueError(
-                f"F_in, F_out, F, K must be positive. Got {F_in}, {F_out}, {F}, {K}."
-            )
-        if n_layers_gnn <= 0:
-            raise ValueError(f"n_layers must be positive. Got {n_layers_gnn}.")
-        if n_layers_mlp <= 0:
-            raise ValueError(f"n_layers_mlp must be positive. Got {n_layers_mlp}.")
-        if activation not in self.activations:
-            raise ValueError(f"Unknown activation {activation}.")
-        activation_: nn.Module = self.activations[activation]
-
-        encoder_layers = []
-        for i in range(n_layers_mlp):
-            encoder_layers += [
-                nn.Linear(F_in if i == 0 else F, F),
-                activation_,
-                nn.Dropout(dropout),
-            ]
-
-        decoder_layers = []
-        for i in range(n_layers_mlp):
-            decoder_layers += [
-                nn.Linear(F, F_out if i == n_layers_mlp - 1 else F),
-            ]
-            if i < n_layers_mlp - 1:
-                decoder_layers += [activation_, nn.Dropout(dropout)]
-
-        gnn_layers = []
-        for i in range(n_layers_gnn):
-            gnn_layer = {
-                "tag": gnn.TAGConv(F, F, K=K, normalize=True),
-                "gat": gnn.GATv2Conv(F, F),
-            }[architecture]
-            gnn_layers += [
-                (
-                    gnn_layer,
-                    "x, edge_index, edge_weight -> x",
-                ),
-                (activation_ if i < n_layers_gnn - 1 else nn.Identity(), "x -> x"),
-            ]
-
-        self.encoder = nn.Sequential(*encoder_layers)
-        self.gnn = gnn.Sequential("x, edge_index, edge_weight", gnn_layers)
-        self.decoder = nn.Sequential(*decoder_layers)
-
-    def forward(self, x: torch.Tensor, data: BaseData) -> torch.Tensor:
-        x = self.encoder(x)
-        x = self.gnn(x, data.edge_index, data.edge_weight)
-        x = self.decoder(x)
-        return x
 
 
 class GNNActor(nn.Module):
@@ -94,47 +22,36 @@ class GNNActor(nn.Module):
         self,
         state_ndim: int,
         action_ndim: int,
-        n_nodes: int,
-        F: int = 256,
-        K: int = 4,
-        n_layers_gnn: int = 4,
-        n_layers_mlp: int = 1,
+        n_taps: int = 4,
+        n_layers: int = 2,
+        n_channels: int = 32,
+        activation: typing.Union[nn.Module, str] = "leaky_relu",
+        mlp_read_layers: int = 1,
+        mlp_per_gnn_layers: int = 0,
+        mlp_hidden_channels: int = 256,
         dropout: float = 0.0,
-        activation: str = "leaky_relu",
-        architecture: str = "tag",
-        logsigma_scale: float = 3.0,
     ):
         super().__init__()
         self.state_ndim = state_ndim
         self.action_ndim = action_ndim
-        self.n_nodes = n_nodes
-        self.logsigma_scale = logsigma_scale
-        self.gnn_mu = GNN(
+
+        self.gnn = GCN(
             state_ndim,
-            action_ndim,
-            F,
-            K,
-            n_layers_gnn,
-            n_layers_mlp,
-            activation=activation,
-            architecture=architecture,
-            dropout=dropout,
-        )
-        self.gnn_sigma = GNN(
-            state_ndim,
-            action_ndim,
-            F,
-            K,
-            n_layers_gnn,
-            n_layers_mlp,
-            activation=activation,
-            architecture=architecture,
-            dropout=dropout,
+            action_ndim * 2,
+            n_taps,
+            n_layers,
+            n_channels,
+            activation,
+            mlp_read_layers,
+            mlp_per_gnn_layers,
+            mlp_hidden_channels,
+            dropout,
         )
 
     def forward(self, state: torch.Tensor, data: BaseData):
-        mu = self.gnn_mu.forward(state, data)
-        sigma = self.gnn_sigma.forward(state, data).abs()
+        action = self.gnn.forward(state, data.edge_index, data.edge_attr)
+        mu = action[:, : self.action_ndim]
+        sigma = F.softplus(action[:, self.action_ndim :])
         return mu, sigma
 
     def policy(
@@ -174,29 +91,29 @@ class GNNCritic(nn.Module):
         self,
         state_ndim: int,
         action_ndim: int,
-        n_nodes: int,
-        F: int = 128,
-        K: int = 2,
+        n_taps: int = 4,
+        n_layers: int = 2,
+        n_channels: int = 32,
+        activation: typing.Union[nn.Module, str] = "leaky_relu",
+        mlp_read_layers: int = 1,
+        mlp_per_gnn_layers: int = 0,
+        mlp_hidden_channels: int = 256,
         dropout: float = 0.0,
-        n_layers_gnn: int = 4,
-        n_layers_mlp: int = 1,
-        activation: str = "leaky_relu",
-        architecture: str = "tag",
     ):
         super().__init__()
         self.state_ndim = state_ndim
         self.action_ndim = action_ndim
-        self.n_nodes = n_nodes
-        self.gnn = GNN(
+        self.gnn = GCN(
             state_ndim + action_ndim,
             1,
-            F,
-            K,
-            n_layers_gnn,
-            n_layers_mlp,
-            activation=activation,
-            architecture=architecture,
-            dropout=dropout,
+            n_taps,
+            n_layers,
+            n_channels,
+            activation,
+            mlp_read_layers,
+            mlp_per_gnn_layers,
+            mlp_hidden_channels,
+            dropout,
         )
 
     def forward(
@@ -206,7 +123,7 @@ class GNNCritic(nn.Module):
         data: BaseData,
     ) -> torch.Tensor:
         x = torch.cat([state, action], dim=-1)
-        y = self.gnn(x, data)
+        y = self.gnn.forward(x, data.edge_index, data.edge_attr)
         return scatter_mean(y, data.batch, dim=0)
 
 
@@ -217,62 +134,57 @@ class MotionPlanningActorCritic(pl.LightningModule):
 
     def __init__(
         self,
-        F: int = 128,
-        K: int = 8,
-        n_layers_gnn: int = 1,
-        n_layers_mlp: int = 2,
+        n_taps: int = 4,
+        n_layers: int = 2,
+        n_channels: int = 32,
+        activation: typing.Union[nn.Module, str] = "leaky_relu",
+        mlp_read_layers: int = 1,
+        mlp_per_gnn_layers: int = 0,
+        mlp_hidden_channels: int = 256,
+        dropout: float = 0.0,
         lr: float = 0.0001,
         weight_decay: float = 0.0,
         batch_size: int = 32,
         gamma=0.95,
         max_steps=200,
-        dropout: float = 0.0,
-        activation: str = "leaky_relu",
-        architecture: str = "tag",
         n_agents: int = 100,
         scenario: str = "uniform",
-        optimizer: str = "adamw",
         **kwargs,
     ):
         super().__init__()
         self.save_hyperparameters(ignore="kwargs")
 
-        self.F = F
-        self.K = K
-        self.n_layers_gnn = n_layers_gnn
-        self.n_layers_mlp = n_layers_mlp
         self.lr = lr
         self.weight_decay = weight_decay
         self.batch_size = batch_size
         self.gamma = gamma
         self.max_steps = max_steps
-        self.optimizer = optimizer
         self.dropout = dropout
 
         self.env = MotionPlanning(n_agents=n_agents, scenario=scenario)
         self.actor = GNNActor(
             self.env.observation_ndim,
             self.env.action_ndim,
-            self.env.n_nodes,
-            F=F,
-            K=K,
-            n_layers_gnn=n_layers_gnn,
-            n_layers_mlp=n_layers_mlp,
-            activation=activation,
-            architecture=architecture,
-            dropout=dropout,
+            n_taps,
+            n_layers,
+            n_channels,
+            activation,
+            mlp_read_layers,
+            mlp_per_gnn_layers,
+            mlp_hidden_channels,
+            dropout,
         )
         self.critic = GNNCritic(
             self.env.observation_ndim,
             self.env.action_ndim,
-            self.env.n_nodes,
-            F=F,
-            K=K,
-            n_layers_gnn=n_layers_gnn,
-            n_layers_mlp=n_layers_mlp,
-            activation=activation,
-            architecture=architecture,
-            dropout=dropout,
+            n_taps,
+            n_layers,
+            n_channels,
+            activation,
+            mlp_read_layers,
+            mlp_per_gnn_layers,
+            mlp_hidden_channels,
+            dropout,
         )
 
     def rollout_start(self):
@@ -300,9 +212,7 @@ class MotionPlanningActorCritic(pl.LightningModule):
 
             # sample action
             data.mu, data.sigma = self.actor(data.state, data)
-            data.action = (
-                self.actor.policy(data.mu, data.sigma) if self.training else data.mu
-            )
+
             # take step
             data, next_state, reward, done = self.rollout_step(data)
 
@@ -344,26 +254,14 @@ class MotionPlanningActorCritic(pl.LightningModule):
         return opt_actor, opt_critic
 
     def configure_optimizers(self):
-        if self.optimizer == "adamw":
-            return [
-                torch.optim.AdamW(
-                    self.actor.parameters(), lr=self.lr, weight_decay=self.weight_decay
-                ),
-                torch.optim.AdamW(
-                    self.critic.parameters(), lr=self.lr, weight_decay=self.weight_decay
-                ),
-            ]
-        elif self.optimizer == "sgd":
-            return [
-                torch.optim.SGD(
-                    self.actor.parameters(), lr=self.lr, weight_decay=self.weight_decay
-                ),
-                torch.optim.SGD(
-                    self.critic.parameters(), lr=self.lr, weight_decay=self.weight_decay
-                ),
-            ]
-        else:
-            raise ValueError(f"Unknown optimizer: {self.optimizer}")
+        return [
+            torch.optim.AdamW(
+                self.actor.parameters(), lr=self.lr, weight_decay=self.weight_decay
+            ),
+            torch.optim.AdamW(
+                self.critic.parameters(), lr=self.lr, weight_decay=self.weight_decay
+            ),
+        ]
 
     def to_data(self, state, adjacency) -> BaseData:
         if isinstance(adjacency, list):
