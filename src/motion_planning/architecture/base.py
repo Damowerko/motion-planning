389e9eb797101918dbee4b5ea667f76ef3f8ecd5
Damian Owerko <damowerko@gmail.com>
2025-01-17 19:19:45 -0500

    Check compilation status of each submodule individually

--- src/motion_planning/architecture/base.py
+++ src/motion_planning/architecture/base.py
@@ -39,13 +39,14 @@ class ActorCritic(nn.Module):
         return self.critic(action, data)
 
     def state_dict(self, *args, **kwargs):
-        if not hasattr(self.actor, "_orig_mod") or not hasattr(
-            self.critic, "_orig_mod"
-        ):
+        if hasattr(self.actor, "_orig_mod"):
             actor_state_dict = self.actor._orig_mod.state_dict(*args, **kwargs)
-            critic_state_dict = self.critic._orig_mod.state_dict(*args, **kwargs)
         else:
             actor_state_dict = self.actor.state_dict(*args, **kwargs)
+
+        if hasattr(self.critic, "_orig_mod"):
+            critic_state_dict = self.critic._orig_mod.state_dict(*args, **kwargs)
+        else:
             critic_state_dict = self.critic.state_dict(*args, **kwargs)
         return {
             "actor": actor_state_dict,
