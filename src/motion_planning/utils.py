1f447f64c9c9b6d6bbc3f3bc6a6273fbaa066fab
Damian Owerko <damowerko@gmail.com>
2025-02-15 12:28:21 -0500

    Remove tensorboard from utils

--- src/motion_planning/utils.py
+++ src/motion_planning/utils.py
@@ -5,7 +5,6 @@ from tempfile import TemporaryDirectory
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
-from torch.utils.tensorboard import SummaryWriter
 from wandb.sdk.wandb_run import Run
 
 from motion_planning.architecture import GNNActorCritic, TransformerActorCritic
@@ -16,33 +15,6 @@ from motion_planning.lightning import (
     MotionPlanningImitation,
     MotionPlanningTD3,
 )
-
-
-class TensorboardHistogramLogger(pl.Callback):
-    def __init__(self, every_n_steps=1000):
-        super().__init__()
-        self.every_n_steps = every_n_steps
-        self.last_called = 0
-
-    @staticmethod
-    def _get_writer(pl_module: pl.LightningModule) -> SummaryWriter:
-        writer = pl_module.logger.experiment  # type: ignore
-        assert isinstance(writer, SummaryWriter), "Expexted logger to be SummaryWriter."
-        return writer
-
-    def on_before_optimizer_step(self, trainer, pl_module, optimizer, opt_idx):
-        writer = self._get_writer(pl_module)
-        if trainer.global_step - self.last_called >= self.every_n_steps:
-            self.last_called = trainer.global_step
-            for name, params in pl_module.named_parameters():
-                writer.add_histogram("param/" + name, params, trainer.global_step)
-                writer.add_histogram("grad/" + name, params.grad, trainer.global_step)
-
-    def on_validation_epoch_start(self, trainer, pl_module):
-        writer = self._get_writer(pl_module)
-        assert isinstance(writer, SummaryWriter), "Expexted logger to be SummaryWriter."
-        for name, param in pl_module.named_parameters():
-            writer.add_histogram("param/" + name, param, trainer.current_epoch)
 
 
 def get_architecture_cls(model_str) -> typing.Type[ActorCritic]:
